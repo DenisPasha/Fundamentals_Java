e2c9379283599f517b903dd1540cb56cfb920a08
Denis Pasha <111784197+DenisPasha@users.noreply.github.com>
2022-11-03 18:58:52 +0000

    update

--- src/P07AssociativeArrays/Exercise/P03Orders.java
+++ src/P07AssociativeArrays/Exercise/P03Orders.java
@@ -1,11 +1,54 @@
 package P07AssociativeArrays.Exercise;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class P03Orders {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String command = scanner.nextLine();
+        Map<String, Integer> mapQuantity = new LinkedHashMap<>();
+        Map<String, Double> mapPrice = new LinkedHashMap<>();
+        Map<String, Double> mapFinal = new LinkedHashMap<>();
 
+
+        while (!command.equals("buy")) {
+
+            String product = command.split(" ")[0];
+            double price = Double.parseDouble(command.split(" ")[1]);
+            int quantity = Integer.parseInt(command.split(" ")[2]);
+
+            //quantity maps filling
+            mapQuantity.putIfAbsent(product, 0);
+            mapQuantity.put(product, mapQuantity.get(product) + quantity);
+
+            //price map filling
+            mapPrice.put(product, price);
+
+
+            command = scanner.nextLine();
+        }
+
+        boolean isEnough=false;
+        for (Map.Entry<String, Integer> entry : mapQuantity.entrySet()) {
+
+            for (Map.Entry<String, Double> doubleEntry : mapPrice.entrySet()) {
+                double sum=0;
+                if (doubleEntry.getKey().equals(entry.getKey())){
+                     sum = entry.getValue() * doubleEntry.getValue();
+                    mapFinal.putIfAbsent(entry.getKey(), sum);
+                }
+
+
+
+
+            }
+
+        }
+
+        mapFinal.entrySet().forEach(stringDoubleEntry -> System.out.printf("%s -> %.2f%n",stringDoubleEntry.getKey(),stringDoubleEntry.getValue()));
     }
 }
