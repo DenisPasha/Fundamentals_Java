b14ddac27e264ff203e2c4feef7b74ad29d04d95
Denis Pasha <111784197+DenisPasha@users.noreply.github.com>
2022-12-02 18:44:49 +0000

    update

--- src/FinalExamExercises/P02/P03PlantDiscovery.java
+++ src/FinalExamExercises/P02/P03PlantDiscovery.java
@@ -1,38 +1,36 @@
 package FinalExamExercises.P02;
 
 import java.util.*;
-import java.util.regex.Matcher;
+
 
 public class P03PlantDiscovery {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int n=Integer.parseInt(scanner.nextLine());
-        Map<String,List<Double>> map=new LinkedHashMap<>();
+        Map<String,List<Double>>map=new LinkedHashMap<>();
 
         for (int i = 1; i <=n ; i++) {
-            String input=scanner.nextLine();
-            String[]inputArr= input.split("<->");
 
-            String plantName=inputArr[0];
-            double rarity= Integer.parseInt(inputArr[1]);
+            String[] inputArr=scanner.nextLine().split("<->");
+            String plant=inputArr[0];
+            double rarity= Double.parseDouble(inputArr[1]);
 
-            map.putIfAbsent(plantName,new ArrayList<>());
-            map.get(plantName).add(rarity);
+            map.putIfAbsent(plant,new ArrayList<>());
+            map.get(plant).add(rarity);
 
         }
+
         String command=scanner.nextLine();
 
         while (!command.equals("Exhibition")){
+            String[] commandArr=command.split(": ");
+            String mainCommand=commandArr[0];
 
-            if (command.contains("Rate")){
-
-                String[] commands=command.split(": ");
-                String temp= commands[1];
-
-                String[]commandArr=temp.split(" - ");
-                String plant=commandArr[0];
-                double rating= Integer.parseInt(commandArr[1]);
+            if (mainCommand.equals("Rate")){
+                String[] plantAndRating=commandArr[1].split(" - ");
+                String plant = plantAndRating[0];
+                double rating = Double.parseDouble(plantAndRating[1]);
 
                 if (map.containsKey(plant)){
                     map.get(plant).add(rating);
@@ -41,33 +39,28 @@ public class P03PlantDiscovery {
                 }
 
 
-
-            } else if (command.contains("Update")) {
-
-                String[] commands=command.split(": ");
-                String temp= commands[1];
-
-                String[]commandArr=temp.split(" - ");
-                String plant=commandArr[0];
-                double  rarity= Integer.parseInt(commandArr[1]);
+            }else if (mainCommand.equals("Update")){
+                String[] plantAndRarity=commandArr[1].split(" - ");
+                String plant = plantAndRarity[0];
+                double newRarity = Double.parseDouble(plantAndRarity[1]);
 
                 if (map.containsKey(plant)){
-                    map.get(plant).set(0,rarity);
+                    map.get(plant).set(0,newRarity);
                 }else {
                     System.out.println("error");
                 }
 
 
-
-            } else if (command.contains("Reset")) {
-                String[] commandArr=command.split(": ");
+            } else if (mainCommand.equals("Reset")) {
                 String plant=commandArr[1];
 
                 if (map.containsKey(plant)){
-                    for (int i = 1; i < map.get(plant).size(); i++) {
-                        map.get(plant).remove(i);
-                    }
+                    int sizeOfList = map.get(plant).size();
 
+                    while (sizeOfList > 1){
+                        map.get(plant).remove(sizeOfList-1);
+                        sizeOfList--;
+                    }
                 }else {
                     System.out.println("error");
                 }
@@ -75,31 +68,33 @@ public class P03PlantDiscovery {
 
 
             }
-
-
             command=scanner.nextLine();
         }
 
-        double averageRating=0;
         System.out.println("Plants for the exhibition:");
+
         for (Map.Entry<String, List<Double>> entry : map.entrySet()) {
 
-            String plant= entry.getKey();
+            double sum = 0;
 
-            int totalRating=0;
-            int length=map.get(plant).size()-1;
-
-            for (int i = 1; i <map.get(plant).size() ; i++) {
-                totalRating+=map.get(plant).get(i);
+            for (int i = 1; i < entry.getValue().size(); i++) {
+                double currentRating = entry.getValue().get(i);
+                sum+=currentRating;
             }
-            if (length!=0){
-                averageRating=totalRating*1.0/length;
+            double averageRating = 0;
+            if (sum > 0){
+                int divideBy = entry.getValue().size();
+                int last = divideBy - 1;
+                 averageRating = sum / last ;
             }
 
+            String plantName = entry.getKey();
+            double rarity = entry.getValue().get(0);
 
-            System.out.printf("- %s; Rarity: %.0f; Rating: %.2f%n",plant,map.get(plant).get(0),averageRating);
+            System.out.printf("- %s; Rarity: %.0f; Rating: %.2f%n",plantName,rarity,averageRating);
 
         }
+
 
     }
 }
