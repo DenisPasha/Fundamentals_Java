c201fe0c5ae469bbfd15be98c0cb299ebad7fd80
Denis Pasha <111784197+DenisPasha@users.noreply.github.com>
2022-12-03 12:06:33 +0000

    update

--- src/FinalExamExercises/P03/P02MirrorWords.java
+++ src/FinalExamExercises/P03/P02MirrorWords.java
@@ -10,60 +10,57 @@ public class P02MirrorWords {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String input = scanner.nextLine();
 
-        String regex="([@#])(?<validWord>[A-Za-z]{3,})\\1([@#])(?<validWord2>[A-Za-z]{3,})\\1";
+        String input = scanner.nextLine();
+        String regex = "(@|#)(?<wodOne>[A-Za-z]{3,})\\1\\1(?<wordTwo>[A-Za-z]{3,})\\1";
         Pattern pattern= Pattern.compile(regex);
         Matcher matcher= pattern.matcher(input);
 
-        List<String>wordsList=new ArrayList<>();
-        List<String>validWordList=new ArrayList<>();
-
+        List<String>mirrorWordsList=new ArrayList<>();
+        List<String>validPairsList=new ArrayList<>();
+        boolean validPairsFind=false;
 
         while (matcher.find()){
-            wordsList.add(matcher.group("validWord"));
-            wordsList.add(matcher.group("validWord2"));
-        }
-
-        for (int i = 0; i < wordsList.size()-1; i+=2) {
-            String currentWord= wordsList.get(i);
-            String nextWord= wordsList.get(i+1);
+            validPairsFind=true;
+            String firstWord= matcher.group("wodOne");
+            String secondWord= matcher.group("wordTwo");
+            validPairsList.add(firstWord);
+            validPairsList.add(secondWord);
 
             StringBuilder reversed=new StringBuilder();
-            for (int j = nextWord.length()-1; j >=0; j--) {
-                char currentChar=nextWord.charAt(j);
-                reversed.append(currentChar);
+            for (int i = secondWord.length()-1; i >=0 ; i--) {
+                char current = secondWord.charAt(i);
+                reversed.append(current);
             }
-
-            if (currentWord.equals(reversed.toString())){
-                validWordList.add(currentWord);
-                validWordList.add(nextWord);
+            if (firstWord.equals(reversed.toString())){
+                mirrorWordsList.add(firstWord);
+                mirrorWordsList.add(secondWord);
             }
         }
-
-        if (wordsList.size()<=0){
+        if (!validPairsFind){
             System.out.println("No word pairs found!");
-            System.out.println("No mirror words!");
         }else {
-            System.out.printf("%d word pairs found!%n",wordsList.size()/2);
-            if (validWordList.size()<=0){
-                System.out.println("No mirror words!");
-            }else {
-                System.out.printf("The mirror words are:%n");
-                for (int i = 0; i < validWordList.size()-3; i+=2) {
-                    String currentWord= validWordList.get(i);
-                    String nextWord= validWordList.get(i+1);
+            System.out.printf("%d word pairs found!%n",validPairsList.size()/2);
+        }
 
-                    System.out.printf("%s <=> %s, ",currentWord,nextWord);
-                }
-                for (int i = validWordList.size()-2; i < validWordList.size()-1; i++) {
-                    String currentWord= validWordList.get(i);
-                    String nextWord= validWordList.get(i+1);
 
-                    System.out.printf("%s <=> %s",currentWord,nextWord);
-                }
+        if (mirrorWordsList.size() > 0){
+            System.out.println("The mirror words are:");
+            for (int i = 0; i < mirrorWordsList.size()-3; i++) {
+                if (i % 2 ==0){
+                    System.out.printf("%s <=> %s, ",mirrorWordsList.get(i),mirrorWordsList.get(i+1));
+                 }
+
 
             }
+
+            for (int i = mirrorWordsList.size()-3; i < mirrorWordsList.size()-1; i++) {
+                if (i % 2 ==0){
+                    System.out.printf("%s <=> %s",mirrorWordsList.get(i),mirrorWordsList.get(i+1));
+                }
+            }
+        }else {
+            System.out.println("No mirror words!");
         }
 
     }
